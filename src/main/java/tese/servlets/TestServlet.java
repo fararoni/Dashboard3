799e0f2f83c69f9adbc1f917e6d848b741616be8
fararoni <fararoni@gmail.com>
2023-03-12 03:19:12 -0600

    Version DAO

--- src/main/java/tese/servlets/TestServlet.java
+++ src/main/java/tese/servlets/TestServlet.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package tese.servlets;
 
 import java.io.IOException;
@@ -19,22 +15,8 @@ import tese.dao.DatabaseDAO;
 import tese.dao.UsuarioDAO;
 import tese.pojo.Usuario;
 
-/**
- *
- * @author Isai
- */
 @WebServlet(name = "TestServlet", urlPatterns = {"/TestServlet"})
 public class TestServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
